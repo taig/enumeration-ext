8ca0ff3376c7bf13fe2fb13fa8d83a697eef6b56
Niklas Klein <mail@taig.io>
2024-07-07 13:40:02 +0200

    Revert "Add Mapping.Inject and .Project"
    This reverts commit 7fc2c7ad5bb5e668fc5e8f2ca6fa1f5a3133d086.

--- modules/core/src/main/scala/io/taig/enumeration/ext/Mapping.scala
+++ modules/core/src/main/scala/io/taig/enumeration/ext/Mapping.scala
@@ -7,54 +7,10 @@ import cats.data.NonEmptyMap
 import scala.annotation.targetName
 import cats.Order
 
-sealed abstract class Mapping[A, B] extends Inject[A, B], Mapping.Inject[A, B], Mapping.Project[A, B]
+sealed abstract class Mapping[A, B] extends Inject[A, B]:
+  def values: List[A]
 
 object Mapping:
-  sealed trait Inject[A, B]:
-    def values: List[A]
-
-    def inj: A => B
-
-  object Inject:
-    inline def apply[A, B](using mapping: Mapping.Inject[A, B]): Mapping.Inject[A, B] = mapping
-
-    def enumeration[A, B: Order](f: A => B)(using values: EnumerationValues.Aux[A, A]): Mapping.Inject[A, B] =
-      new Mapping.Inject[A, B]:
-        override def values: List[A] = valuesOf[A].toList
-        override def inj: A => B = f
-
-    def constant[A <: B: ValueOf, B: Eq]: Mapping.Inject[A, B] = new Mapping.Inject[A, B]:
-      override def values: List[A] = List(valueOf[A])
-      override def inj: A => B = identity
-
-    @targetName("constantOf")
-    def constant[A: Eq](value: A & Singleton): Mapping.Inject[value.type, A] = constant[value.type, A]
-
-  sealed trait Project[A, B]:
-    def values: List[A]
-
-    def prj: B => Option[A]
-
-  object Project:
-    inline def apply[A, B](using mapping: Mapping.Project[A, B]): Mapping.Project[A, B] = mapping
-
-    def enumeration[A, B: Order](f: A => B)(using values: EnumerationValues.Aux[A, A]): Mapping.Project[A, B] =
-      new Mapping.Project[A, B]:
-        val valuesNel = valuesOf[A]
-        val lookup: NonEmptyMap[B, A] =
-          NonEmptyMap.of((f(valuesNel.head), valuesNel.head), valuesNel.tail.map(a => (f(a), a))*)
-
-        override def values: List[A] = valuesNel.toList
-        override def prj: B => Option[A] = lookup.apply
-
-    def constant[A <: B: ValueOf, B: Eq]: Mapping.Project[A, B] = new Mapping.Project[A, B]:
-      val a: A = valueOf[A]
-      override def values: List[A] = List(a)
-      override def prj: B => Option[A] = b => Option.when(b === a)(a)
-
-    @targetName("constantOf")
-    def constant[A: Eq](value: A & Singleton): Mapping.Project[value.type, A] = constant[value.type, A]
-
   inline def apply[A, B](using mapping: Mapping[A, B]): Mapping[A, B] = mapping
 
   def enumeration[A, B: Order](f: A => B)(using values: EnumerationValues.Aux[A, A]): Mapping[A, B] = new Mapping[A, B]:
@@ -67,9 +23,10 @@ object Mapping:
     override def prj: B => Option[A] = lookup.apply
 
   def constant[A <: B: ValueOf, B: Eq]: Mapping[A, B] = new Mapping[A, B]:
-    override def values: List[A] = List(valueOf[A])
+    val a: A = valueOf[A]
+    override def values: List[A] = List(a)
     override def inj: A => B = identity
-    override def prj: B => Option[A] = b => Option.when(b === valueOf[A])(valueOf[A])
+    override def prj: B => Option[A] = b => Option.when(b === a)(a)
 
   @targetName("constantOf")
   def constant[A: Eq](value: A & Singleton): Mapping[value.type, A] = constant[value.type, A]
