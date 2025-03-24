deb66ee293084c7a56174b9b22fab861f30d0b04
Niklas Klein <mail@taig.io>
2025-03-24 10:58:40 +0100

    Fix ciris given instance broken inference

--- modules/ciris/src/main/scala/io/taig/enumeration/ext/ciris.scala
+++ modules/ciris/src/main/scala/io/taig/enumeration/ext/ciris.scala
@@ -6,12 +6,12 @@ import cats.kernel.Order
 import cats.syntax.all.*
 
 trait ciris:
-  given conficDecoder[A, B, C: Show](using mapping: Mapping[B, C], decoder: ConfigDecoder[A, C]): ConfigDecoder[A, B] =
+  given conficDecoder[A, B, C: Show](using mapping: Mapping[B, C])(using decoder: ConfigDecoder[A, C]): ConfigDecoder[A, B] =
     ConfigDecoder[A, C].mapOption(typeName = mapping.values.map(mapping.inj).map(_.show).mkString_("|"))(mapping.prj)
 
   def decoderEnumeration[A, B, C: Order: Show](
       f: B => C
   )(using EnumerationValues.Aux[B, B])(using decoder: ConfigDecoder[A, C]): ConfigDecoder[A, B] =
-    conficDecoder(using Show[C], Mapping.enumeration(f), decoder)
+    conficDecoder(using Mapping.enumeration(f))
 
 object ciris extends ciris
