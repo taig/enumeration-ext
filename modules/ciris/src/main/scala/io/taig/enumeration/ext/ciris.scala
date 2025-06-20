a4e431976b5faf7ee984618e3d3fbc70fd2972ac
Niklas Klein <mail@taig.io>
2025-06-20 11:37:55 +0200

    conficDecoder -> configDecoder

--- modules/ciris/src/main/scala/io/taig/enumeration/ext/ciris.scala
+++ modules/ciris/src/main/scala/io/taig/enumeration/ext/ciris.scala
@@ -6,7 +6,7 @@ import cats.kernel.Order
 import cats.syntax.all.*
 
 trait ciris:
-  given conficDecoder[A, B, C: Show](using
+  given configDecoder[A, B, C: Show](using
       mapping: Mapping[B, C]
   )(using decoder: ConfigDecoder[A, C]): ConfigDecoder[A, B] =
     ConfigDecoder[A, C].mapOption(typeName = mapping.values.map(mapping.inj).map(_.show).mkString_("|"))(mapping.prj)
@@ -14,6 +14,6 @@ trait ciris:
   def decoderEnumeration[A, B, C: Order: Show](
       f: B => C
   )(using EnumerationValues.Aux[B, B])(using decoder: ConfigDecoder[A, C]): ConfigDecoder[A, B] =
-    conficDecoder(using Mapping.enumeration(f))
+    configDecoder(using Mapping.enumeration(f))
 
 object ciris extends ciris
